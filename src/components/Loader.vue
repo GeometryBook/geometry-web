c658adee438bd8704281bc8d42308b0ed171c31f
Jun Ambitious <1535820771@qq.com>
2021-08-12 14:28:36 +0800

    抽出Table

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="loader"
       v-loading.fullscreen.lock="loading"
-      element-loading-text="拼命加载中"
+      element-loading-text="加载中"
       element-loading-spinner="el-icon-loading"
       element-loading-background="rgba(0, 0, 0, 0.6)"
       >
